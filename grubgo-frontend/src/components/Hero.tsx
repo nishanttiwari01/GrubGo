bac8c80844277dec90f3621f0fc9a7b1b10b3ffe
nishanttiwari01 <nishanttiwari02192@gmail.com>
2025-09-25 11:44:06 +0530

    test

---
+++ grubgo-frontend/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import gif2 from "../assets/gif3.gif";
+
+const Hero = () => {
+  return (
+    <div>
+      <img src={gif2} className="w-full max-h-[500px] object-cover" />
+    </div>
+  );
+};
+
+export default Hero;
